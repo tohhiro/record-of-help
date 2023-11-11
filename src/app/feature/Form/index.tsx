490f8cb118c2802aa5158636d82c509b24c80e3c
hiroto <tohhiro@gmail.com>
2023-11-11 18:21:17 +0900

    mt、mbをmyにまとめる

--- src/app/feature/Form/index.tsx
+++ src/app/feature/Form/index.tsx
@@ -5,7 +5,7 @@ export default function Form() {
   return (
     <div className="w-100  h-200 m-10 text-center">
       <span className="text-4xl">Form！！</span>
-      <div className="w-80 mt-8 mb-8 m-auto">
+      <div className="w-80 my-8 m-auto">
         <Checkbox id="radio1" label="Radio1" />
         <Checkbox id="radio2" label="Radio2" />
         <Checkbox id="radio3" label="Radio3" />
