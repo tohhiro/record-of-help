a26d4598d88b0744bae20d78855b7604e5f4f5d6
Hiroto Fujimura <33173829+tohhiro@users.noreply.github.com>
2023-11-11 18:24:17 +0900

    update: checkboxにスタイルを追加 (#8)
    * checkboxにスタイル追加
    * FormのCheckboxを整列させるようにstyleを追加

--- src/app/feature/Form/index.tsx
+++ src/app/feature/Form/index.tsx
@@ -5,9 +5,11 @@ export default function Form() {
   return (
     <div className="w-100  h-200 m-10 text-center">
       <span className="text-4xl">Form！！</span>
-      <Checkbox label="Radio" />
-      <Checkbox label="Radio2" />
-      <Checkbox label="Radio3" />
+      <div className="w-80 my-8 m-auto">
+        <Checkbox id="radio1" label="Radio1" />
+        <Checkbox id="radio2" label="Radio2" />
+        <Checkbox id="radio3" label="Radio3" />
+      </div>
       <Button label="Button" type="button" style="primary" />
     </div>
   );
