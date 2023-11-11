6dac68cce455116b55343bd14a5d768bb62a2694
hiroto <tohhiro@gmail.com>
2023-11-11 13:41:18 +0900

    Checkboxコンポーネントのアウトラインを追加

--- src/app/feature/Form/index.tsx
+++ src/app/feature/Form/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from "../../components/Button";
+import { Checkbox } from "../../components/Checkbox";
 
 export default function Form() {
   return (
     <div className="w-100  h-200 m-10 text-center">
       <span className="text-4xl">Form！！</span>
+      <Checkbox label="Radio" />
       <Button label="Button" type="button" style="primary" />
     </div>
   );
