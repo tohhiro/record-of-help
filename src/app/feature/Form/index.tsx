1e1eb89bffb437e582f4c7c2ffddc28b9a3eae3e
hiroto <tohhiro@gmail.com>
2023-11-11 18:20:15 +0900

    Checkboxを整列させるようにstyleを追加

--- src/app/feature/Form/index.tsx
+++ src/app/feature/Form/index.tsx
@@ -5,9 +5,11 @@ export default function Form() {
   return (
     <div className="w-100  h-200 m-10 text-center">
       <span className="text-4xl">Form！！</span>
-      <Checkbox id="radio1" label="Radio1" />
-      <Checkbox id="radio2" label="Radio2" />
-      <Checkbox id="radio3" label="Radio3" />
+      <div className="w-80 mt-8 mb-8 m-auto">
+        <Checkbox id="radio1" label="Radio1" />
+        <Checkbox id="radio2" label="Radio2" />
+        <Checkbox id="radio3" label="Radio3" />
+      </div>
       <Button label="Button" type="button" style="primary" />
     </div>
   );
