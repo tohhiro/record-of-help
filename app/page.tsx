2e39d71332f56d57f44c0014a7b2a7d5382fdfe2
hiroto <tohhiro@gmail.com>
2023-11-14 09:05:21 +0900

    ヘッダーを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Dashbord from "./feature/Dashbord";
 import Form from "./feature/Form";
+import { Header } from "./feature/Header";
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <Dashbord />
       <Form />
     </main>
