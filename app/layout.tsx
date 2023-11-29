489d0bdeccbc8531e42f137aab4e2e6c4dab0bc6
hiroto <tohhiro@gmail.com>
2023-11-30 08:52:57 +0900

    layoutのchildrenの型をPropsWithChildrenに変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { PropsWithChildren } from "react";
 import "./globals.css";
 
 export const metadata = {
@@ -5,11 +6,7 @@ export const metadata = {
   description: "This App is recording help by your children",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ja">
       <body>{children}</body>
