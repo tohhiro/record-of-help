20239360d77147077f9a308877d3c5e9e3730674
hiroto <tohhiro@gmail.com>
2023-11-29 09:21:37 +0900

    FormのStorybookを追加

---
+++ app/feature/Form/index.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../globals.css";
+import { Form } from ".";
+
+export default {
+  title: "app/feature/Form",
+  component: Form,
+};
+  
+  export const Default: React.FC = (): JSX.Element => {
+    return <Form />;
+  };
+  
