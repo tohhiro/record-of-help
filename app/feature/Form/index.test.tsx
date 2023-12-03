8e4eae0d74134503cdafbd186f0965b1c63fe06e
hiroto <tohhiro@gmail.com>
2023-12-03 13:00:44 +0900

    FormのcheckboxのuserEventを含むUTを追加

--- app/feature/Form/index.test.tsx
+++ app/feature/Form/index.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
-import { Form, Props } from ".";
+import { Form, Props, helps } from ".";
 
 describe("Form", () => {
     describe("radio",()=>{
@@ -27,6 +27,29 @@ describe("Form", () => {
                 const radioButton = screen.getByRole("radio", { name: value });
                 userEvent.click(radioButton)
                 await waitFor(() => expect(radioButton).toHaveAttribute("checked", true));
+            })
+        });
+    })
+    describe("checkbox",()=>{
+        test("checkboxが5個レンダリングされる", () => {
+            render(<Form />);
+            const checkboxes = screen.getAllByRole("checkbox");
+            expect(checkboxes).toHaveLength(helps.length);
+        });
+        test("checkboxのvalue属性が正しく設定されている", () => {
+            render(<Form />);
+            helps.forEach((help) => {
+                const checkbox = screen.getByRole("checkbox", { name: help.label });
+                expect(checkbox).toHaveAttribute("value", help.id);
+            })
+        });
+        test("checkboxのチェックを入れると、チェックされたcheckboxの属性がcheckedになっている", async () => {
+            render(<Form />);
+            const user = userEvent.setup()
+            helps.forEach(async (help) => {
+                const checkbox = screen.getByRole("checkbox", { name: help.label });
+                userEvent.click(checkbox)
+                await waitFor(() => expect(checkbox).toHaveAttribute("checked", true));
             })
         });
     })
