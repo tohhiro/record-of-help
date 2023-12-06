32e2d4b19ca4dbe4201fc151702a23447226722e
hiroto <tohhiro@gmail.com>
2023-12-06 09:54:29 +0900

    Inputコンポーネントを追加

---
+++ app/components/Input/index.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Input, Props } from ".";
+
+export default {
+  title: "app/components/Input",
+  component: Input,
+};
+
+const baseMockData: Props = {
+    id: "input",
+    label: "Inputラベル",
+    type: "text",
+    onClick: () => {},
+};
+
+export const Default: React.FC = (): JSX.Element => {
+    const mockData: Props = {...baseMockData, disabled: false}
+    return <Input {...mockData} />;
+};
+
+export const password: React.FC = (): JSX.Element => {
+    const mockData: Props = {...baseMockData, type: "password", disabled: false}
+    return <Input {...mockData} />;
+};
+
+export const Disabled: React.FC = (): JSX.Element => {
+    const mockData: Props = {...baseMockData, disabled: true}
+    return <Input {...mockData} />;
+};
