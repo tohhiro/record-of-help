e14669cb9c5e5cda3c242e3d47eb551c4eb0922d
hiroto <tohhiro@gmail.com>
2023-12-25 09:38:05 +0900

    convertするアイテムを追加

--- app/(feature)/Form/convertHelps.ts
+++ app/(feature)/Form/convertHelps.ts
@@ -4,18 +4,21 @@ type HelpsData = {
   dish: boolean;
   curtain: boolean;
   prepareEat: boolean;
+  landry: boolean;
+  towel: boolean;
 };
 
 const initHelpsData: HelpsData = {
   dish: false,
   curtain: false,
   prepareEat: false,
+  landry: false,
+  towel: false,
 };
 
 export const convertHelps = (helps: Helps[], data: Props) => {
   const checkedHelps = helps.reduce(
-    (acc, help) =>
-      (data.helps.includes(help.id) ? { ...acc, [help.id]: true } : { ...acc }),
+    (acc, help) => (data.helps.includes(help.id) ? { ...acc, [help.id]: true } : { ...acc }),
     initHelpsData,
   );
   return checkedHelps;
