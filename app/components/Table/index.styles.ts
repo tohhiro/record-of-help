db3f5e7326ff571b4b35e8afdc136e447f3d4fe9
hiroto <tohhiro@gmail.com>
2024-01-02 13:49:05 +0900

    ドラフト状態でraws_dataがdashboardに表示されるように追加

---
+++ app/components/Table/index.styles.ts
@@ -0,0 +1,8 @@
+export const tableStyles = {
+  container: 'relative overflow-x-auto',
+  table: 'w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400',
+  thead: 'text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400',
+  th: 'px-6 py-3',
+  tr: 'bg-white border-b dark:bg-gray-800 dark:border-gray-700',
+  td: 'px-6 py-4',
+};
---
+++ app/components/Table/index.tsx
@@ -0,0 +1,65 @@
+import React, { forwardRef } from 'react';
+import '../../styles/globals.css';
+import { tableStyles } from './index.styles';
+
+export type Props = {
+  id: string;
+  person: string;
+  curtain: number;
+  dish: number | null;
+  landry: number | null;
+  prepareEat: number | null;
+  towel: number | null;
+  comments: string | null;
+  created_at: string;
+  del_flag: boolean | null;
+};
+
+export const theadNames = {
+  person: '名前',
+  dish: '皿洗い',
+  curtain: 'カーテン開閉',
+  prepareEat: '食事準備',
+  landry: '洗濯物片付け',
+  towel: '芽生タオル',
+  comments: 'コメント',
+  created_at: '日付',
+};
+
+export const Table = forwardRef(({ data }: { data: Props[] | null | undefined }) => {
+  const createTable = (tableData: Props[]) => {
+    return (
+      <table className={tableStyles.table}>
+        <thead className={tableStyles.thead}>
+          <tr>
+            {Object.keys(theadNames).map((key: string, idx) => (
+              <th key={idx} className={tableStyles.th}>
+                {key}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((item) => (
+            <tr key={item.id} className={tableStyles.tr}>
+              <td className={tableStyles.td}>{item.person}</td>
+              <td className={tableStyles.td}>{item.dish}</td>
+              <td className={tableStyles.td}>{item.curtain}</td>
+              <td className={tableStyles.td}>{item.prepareEat}</td>
+              <td className={tableStyles.td}>{item.landry}</td>
+              <td className={tableStyles.td}>{item.towel}</td>
+              <td className={tableStyles.td}>{item.comments}</td>
+              <td className={tableStyles.td}>{item.created_at}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
+  return (
+    <div className={tableStyles.container}>
+      {data ? createTable(data) : <div>データがありません</div>}
+    </div>
+  );
+});
