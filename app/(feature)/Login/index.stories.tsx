e52217c038f9e310ce27fa96abcf5224a0795e51
hiroto <tohhiro@gmail.com>
2024-01-06 15:04:06 +0900

    Loginの返り値のhandlerを追加し、featureのstoryファイルにuseRouterとLoginのhandlerのparameterを追加

--- app/(feature)/Login/index.stories.tsx
+++ app/(feature)/Login/index.stories.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Page from './page';
+import { mockUseSignInHandler } from '../../../mocks/msw/handlers/mockUseSignIn';
 
 export default {
   title: 'app/feature/Login',
   component: Page,
 };
 
-export const Default: React.FC = (): JSX.Element => {
+export const Default = (): JSX.Element => {
   return <Page />;
+};
+
+Default.parameters = {
+  nextjs: {
+    appDirectory: true,
+    navigation: {
+      pathname: '/posts',
+    },
+  },
+  msw: {
+    handlers: [mockUseSignInHandler.start()],
+  },
 };
