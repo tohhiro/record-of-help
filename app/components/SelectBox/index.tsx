1eb736172dfb62cdfafe019085a0bce6aa322cf3
hiroto <tohhiro@gmail.com>
2024-01-17 09:41:40 +0900

    SelectBoxの実装を修正

--- app/components/SelectBox/index.tsx
+++ app/components/SelectBox/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import Select from 'react-select';
 import '../../styles/globals.css';
 import { SelectBoxStyles } from './index.styles';
@@ -8,32 +8,34 @@ export type Props = {
   label: string;
 };
 
-export const SelectBox = ({
-  options,
-  id,
-  isDisabled,
-  label,
-}: {
-  options: Props[];
-  id: string;
-  isDisabled?: boolean;
-  label: string;
-}) => {
-  return (
-    <div>
-      <label htmlFor={id} className={SelectBoxStyles.label}>
-        {label}
-      </label>
-      <Select
-        id={id}
-        name="selects"
-        inputId="selects"
-        isClearable={false}
-        isSearchable={false}
-        className={SelectBoxStyles.container}
-        options={options}
-        isDisabled={isDisabled}
-      />
-    </div>
-  );
-};
+export const SelectBox = forwardRef(
+  (
+    {
+      options,
+      label,
+      id,
+      isDisabled,
+      ...field
+    }: { options: Props[]; label: string; id: string; isDisabled?: boolean },
+    _ref,
+  ) => {
+    return (
+      <div>
+        <label htmlFor={id} className={SelectBoxStyles.label}>
+          {label}
+        </label>
+        <Select
+          id={id}
+          name="selects"
+          inputId="selects"
+          isClearable={false}
+          isSearchable={false}
+          className={SelectBoxStyles.container}
+          options={options}
+          isDisabled={isDisabled}
+          {...field}
+        />
+      </div>
+    );
+  },
+);
