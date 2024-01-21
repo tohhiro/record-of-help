5b7cc7283beefd31a8ee19483cf2df169ac803fd
hiroto <tohhiro@gmail.com>
2024-01-21 19:22:48 +0900

    Headerにnavメニューを渡すように改修

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,23 @@
 import { PropsWithChildren } from 'react';
 import './styles/globals.css';
 import { Header } from './components/Header';
+import type { NavType } from './components/Header';
 
 export const metadata = {
   title: 'Record of Help',
   description: 'This App is recording help by your children',
 };
 
+const navItems: NavType = {
+  Form: './form',
+  Dashboard: './dashboard',
+};
+
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ja">
       <body>
-        <Header />
+        <Header links={navItems} />
         {children}
       </body>
     </html>
