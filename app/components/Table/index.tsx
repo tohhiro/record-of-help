6ada2dc596c572482eff26f039a47f93259604eb
hiroto <tohhiro@gmail.com>
2024-03-06 09:46:32 +0900

    Dashboardのテーブル、及びTableを改修 - Tableは抽象度を持たせフレキシブルに値を受け取れるように改修 - Dashboardのテーブルは別でComponentを切り出しDashboard用にデータを生成して表示

--- app/components/Table/index.tsx
+++ app/components/Table/index.tsx
@@ -1,43 +1,35 @@
 import React, { forwardRef } from 'react';
 import '../../styles/globals.css';
 import { tableStyles } from './index.styles';
-import type { Database } from '../../../supabase/schema';
 
-export type Props = Database['public']['Tables']['raws_data']['Row'][] | null;
+export type PropsTableTd = Record<string, string | number | null>[];
 
 export const Table = forwardRef(
-  ({ thData, tdData }: { thData: { [key: string]: string }; tdData: Props | null }, _ref) => {
-    const createTable = (tableData: Props) => {
-      if (!tableData) return <div>データがありません</div>;
-      const filteredTableData = tableData && tableData.filter((item) => item.del_flag === false);
-      return (
-        <table className={tableStyles.table}>
-          <thead className={tableStyles.thead}>
-            <tr>
-              {Object.keys(thData).map((key: string, idx) => (
-                <th key={`${idx}${key}`} className={tableStyles.th}>
-                  {thData[key]}
-                </th>
+  ({ thData, tdData }: { thData: Record<string, string>; tdData: PropsTableTd | null }, _ref) => {
+    if (!tdData) return <div>データがありません</div>;
+    return (
+      <table className={tableStyles.table}>
+        <thead className={tableStyles.thead}>
+          <tr>
+            {Object.keys(thData).map((key: string, idx) => (
+              <th key={`${idx}${key}`} className={tableStyles.th}>
+                {thData[key]}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {tdData.map((item, idxA) => (
+            <tr key={idxA} className={tableStyles.tr}>
+              {Object.keys(item).map((key, idxB) => (
+                <td key={`${idxB}${key}`} className={tableStyles.td}>
+                  {String(item[key])}
+                </td>
               ))}
             </tr>
-          </thead>
-          <tbody>
-            {filteredTableData.map((item) => (
-              <tr key={item.id} className={tableStyles.tr}>
-                {Object.keys(thData).map((key: string, idx) => (
-                  <td key={`${idx}${key}`} className={tableStyles.td}>
-                    {(key as keyof typeof item) === 'created_at'
-                      ? String(new Date(String(item[key as keyof typeof item])).toLocaleString())
-                      : String(item[key as keyof typeof item])}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      );
-    };
-
-    return createTable(tdData);
+          ))}
+        </tbody>
+      </table>
+    );
   },
 );
