44e483f8916f753a7e37a117c24e14a24658ef47
hiroto <tohhiro@gmail.com>
2024-03-20 17:04:31 +0900

    useSignIn及びuseSignOutの改修 - useSignInの成功の場合のUTを追加 - useSignInが成功の場合、dataを返すように改修 - useSignInの改修に伴うloginページの改修 - useSignOutのテストの軽微な修正 - navHeaderの軽微な修正

--- app/hooks/useSignIn.test.ts
+++ app/hooks/useSignIn.test.ts
@@ -1,10 +1,18 @@
 import { renderHook } from '@testing-library/react';
 import { useSignIn, Props } from './useSignIn';
+import * as Supabase from '../libs/supabase';
+import { AuthTokenResponse, AuthError } from '@supabase/supabase-js';
+
+jest.mock('../libs/supabase');
 
 const mockArgs: Props = {
   email: 'test@gmail.com',
   password: 'password',
 };
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe('useSignIn', () => {
   test('emailとpasswordをセットできる', async () => {
@@ -16,6 +24,48 @@ describe('useSignIn', () => {
 
     expect(signInSpy).toHaveBeenCalledWith({
       ...mockArgs,
+    });
+  });
+
+  test('signOut関数が成功すると、userとsessionが返る', async () => {
+    const data = {
+      data: {
+        user: {
+          id: '1',
+          email: 'test@gmail.com',
+        },
+        session: {
+          access: 'access',
+          refresh: 'refresh',
+          expires_at: 'expires_at',
+        },
+      },
+    };
+    jest.spyOn(Supabase.supabase.auth, 'signInWithPassword').mockResolvedValueOnce({
+      ...data,
+    } as unknown as AuthTokenResponse);
+    const { result } = renderHook(() => useSignIn());
+
+    await expect(result.current.signIn(mockArgs)).resolves.toMatchObject({
+      ...data,
+    });
+  });
+  test('signOut関数失敗するとerrorが返る', async () => {
+    const error = {
+      error: {
+        name: 'AuthError',
+        message: 'Your signOut is encounter the Error.',
+        status: 400,
+        __isAuthError: true,
+      },
+    };
+    jest.spyOn(Supabase.supabase.auth, 'signInWithPassword').mockRejectedValueOnce({
+      ...error,
+    } as unknown as AuthError);
+    const { result } = renderHook(() => useSignIn());
+
+    await expect(result.current.signIn(mockArgs)).rejects.toMatchObject({
+      ...error,
     });
   });
 });
