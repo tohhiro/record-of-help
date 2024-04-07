7da50205ac748aadc274f7afa122bd6fe29bf3df
hiroto <tohhiro@gmail.com>
2024-04-07 19:36:25 +0900

    supabaseListenerをメインのlayoutに配置、middlewareのdraftを作成 - supabaseListenerをメインのlayoutに配置し、ログインしている場合、どの画面でもメールアドレスが表示されるように修正 - middlewareを配置し、将来的にログインしていないとログイン画面しか閲覧できないように修正（現在はgetやpostなどはできないが静的なページは見れる）

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,26 @@
 import { PropsWithChildren } from 'react';
 import './styles/globals.css';
 import { NavHeader } from '@/app/(feature)/navHeader';
+import { headers, cookies } from 'next/headers';
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import type { Database } from '@/supabase/schema';
+import SupabaseListener from '@/app/libs/supabaseListener';
 
 export const metadata = {
   title: 'Record of Help',
   description: 'This App is recording help by your children',
 };
 
-export default function RootLayout({ children }: PropsWithChildren) {
+export default async function RootLayout({ children }: PropsWithChildren) {
+  const supabase = createServerComponentSupabaseClient<Database>({ headers, cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <html lang="ja">
       <body>
+        <SupabaseListener accessToken={session?.access_token} />
         <NavHeader />
         {children}
       </body>
--- app/store/index.ts
+++ app/store/index.ts
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
 type LoginUser = {
   id: string | null;
