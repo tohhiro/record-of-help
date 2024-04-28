87480170afc53a5ebb6a933a06edb2997ca66fce
hiroto <tohhiro@gmail.com>
2024-04-28 18:16:31 +0900

    useFetchMemberのUTを追加 - 引数がとれるか - 取得が成功時のレスポンス - 取得が失敗時のレスポンス

---
+++ app/hooks/useFetchMember.test.ts
@@ -0,0 +1,56 @@
+import { renderHook } from '@testing-library/react';
+import { useFetchMember } from './useFetchMember';
+
+const mockSuccessResponse = {
+  result: {
+    data: [{ admin: true }],
+    error: null,
+  },
+};
+
+const mockErrorResponse = {
+  result: {
+    data: null,
+    error: {
+      message: '失敗',
+      details: '失敗',
+      hint: '失敗',
+      code: '500',
+    },
+  },
+};
+
+describe('useFetchMember', () => {
+  test('引数にemailを渡すことができる', async () => {
+    const { result } = renderHook(() => useFetchMember());
+    const fetchAuth = jest.spyOn(result.current, 'fetchAuth');
+    result.current.fetchAuth({
+      email: 'test@test.com',
+    });
+    expect(fetchAuth).toHaveBeenCalledWith({
+      email: 'test@test.com',
+    });
+  });
+
+  test('レスポンスが成功の場合、resultのdataにはadminがtrueで取得できる', async () => {
+    const { result } = renderHook(() => useFetchMember());
+    jest.spyOn(result.current, 'fetchAuth').mockResolvedValueOnce({ ...mockSuccessResponse });
+
+    const response = await result.current.fetchAuth({ email: 'test@test.com' });
+    expect(response).toEqual({
+      ...mockSuccessResponse,
+    });
+  });
+
+  test('レスポンスが失敗の場合、resultのerrorにオブジェクトが入った状態で取得できる', async () => {
+    const { result } = renderHook(() => useFetchMember());
+    jest.spyOn(result.current, 'fetchAuth').mockResolvedValueOnce({
+      ...mockErrorResponse,
+    });
+
+    const response = await result.current.fetchAuth({ email: 'test@test.com' });
+    expect(response).toEqual({
+      ...mockErrorResponse,
+    });
+  });
+});
