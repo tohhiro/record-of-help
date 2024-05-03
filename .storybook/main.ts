7281ca83c6469f00b652352d4db810f192783564
hiroto <tohhiro@gmail.com>
2024-05-03 16:39:49 +0900

    Storybookが@からの絶対パスが読み込めない問題とHeaderのStoryを修正 - Storybookにて@からのパスで読み込めるように修正 - Headerコンポーネントの権限のmockデータを追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,16 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+const path = require('path');
+
 const config: StorybookConfig = {
   stories: ['../app/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {},
+    },
   ],
   framework: {
     name: '@storybook/nextjs',
@@ -12,6 +18,12 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
+  webpackFinal: async (config: any) => {
+    // Add path aliases
+    config.resolve.alias['@'] = path.resolve(__dirname, '../');
+    return config;
   },
 };
 export default config;
