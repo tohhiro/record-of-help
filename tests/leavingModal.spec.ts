1022bf272970a0c15734ae3f0ecac487cbcfbf47
hiroto <tohhiro@gmail.com>
2024-07-28 17:59:16 +0900

    ログインする関数を追加、confirmが表示されないUTを追加

--- tests/leavingModal.spec.ts
+++ tests/leavingModal.spec.ts
@@ -3,20 +3,31 @@ import { test, expect } from '@playwright/test';
 const email = process.env.EMAIL as string;
 const password = process.env.PASSWORD as string;
 
-test('フォームを入力し、離脱しようとするとconfirmが表示される', async ({ page, baseURL }) => {
+const LoginToForm = async (page: any, baseURL?: string) => {
   await page.goto(`${baseURL}login`);
-
   await page.getByRole('textbox', { name: 'メールアドレス' }).fill(email);
   await page.getByRole('textbox', { name: 'パスワード' }).fill(password);
   await page.getByRole('button', { name: 'ログイン' }).click();
-
   await page.getByRole('link', { name: 'Form' }).click();
+};
+
+test('フォームを入力し、離脱しようとするとconfirmが表示される', async ({ page, baseURL }) => {
+  await LoginToForm(page, baseURL);
 
   await page.getByLabel('eito').check();
-
-  await page.getByRole('link', { name: 'Form' }).click();
+  await page.getByRole('link', { name: 'Dashboard' }).click();
 
   page.on('dialog', (dialog) => {
     expect(dialog.message()).toContain('ページを離れても良いですか？');
+  });
+});
+
+test('フォームを入力せず、離脱しようとするとconfirmは表示されない', async ({ page, baseURL }) => {
+  await LoginToForm(page, baseURL);
+
+  await page.getByRole('link', { name: 'Dashboard' }).click();
+
+  page.on('dialog', (dialog) => {
+    expect(dialog.message()).not.toContain('ページを離れても良いですか？');
   });
 });
