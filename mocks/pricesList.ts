79657656ed98a3e82e35e41f80752403113e2722
hiroto <tohhiro@gmail.com>
2024-08-12 09:47:11 +0900

    `pricesList`をfetchしたときのmockデータを追加 - UTに利用する目的でfetchしたときのmockデータを追加

--- mocks/pricesList.ts
+++ mocks/pricesList.ts
@@ -1,3 +1,5 @@
+import { PricesHelpsList } from '@/app/types';
+
 export const mockPricesList = [
   {
     id: 'dish',
@@ -30,3 +32,99 @@ export const mockPricesList = [
     value: 50,
   },
 ];
+
+export const mockPricesListRaw: PricesHelpsList = {
+  data: [
+    {
+      created_at: '2021-10-01T00:00:00.000000Z',
+      help: 'dish',
+      id: '1',
+      label: '皿洗い',
+      update_at: '2021-10-01T00:00:00.000000Z',
+      prices_list: [
+        {
+          created_at: '2021-10-01T00:00:00.000000Z',
+          help_id: null,
+          id: 1,
+          price: 30,
+          update_at: '2021-10-01T00:00:00.000000Z',
+        },
+      ],
+    },
+    {
+      created_at: '2021-10-01T00:00:00.000000Z',
+      help: 'curtain',
+      id: '2',
+      label: 'カーテン',
+      update_at: '2021-10-01T00:00:00.000000Z',
+      prices_list: [
+        {
+          created_at: '2021-10-01T00:00:00.000000Z',
+          help_id: null,
+          id: 2,
+          price: 10,
+          update_at: '2021-10-01T00:00:00.000000Z',
+        },
+      ],
+    },
+    {
+      created_at: '2021-10-01T00:00:00.000000Z',
+      help: 'prepareEat',
+      id: '3',
+      label: '食事準備',
+      update_at: '2021-10-01T00:00:00.000000Z',
+      prices_list: [
+        {
+          created_at: '2021-10-01T00:00:00.000000Z',
+          help_id: null,
+          id: 3,
+          price: 20,
+          update_at: '2021-10-01T00:00:00.000000Z',
+        },
+      ],
+    },
+    {
+      created_at: '2021-10-01T00:00:00.000000Z',
+      help: 'landry',
+      id: '4',
+      label: '洗濯物片付け',
+      update_at: '2021-10-01T00:00:00.000000Z',
+      prices_list: [
+        {
+          created_at: '2021-10-01T00:00:00.000000Z',
+          help_id: null,
+          id: 4,
+          price: 20,
+          update_at: '2021-10-01T00:00:00.000000Z',
+        },
+      ],
+    },
+    {
+      created_at: '2021-10-01T00:00:00.000000Z',
+      help: 'special',
+      id: '5',
+      label: 'スペシャル',
+      update_at: '2021-10-01T00:00:00.000000Z',
+      prices_list: [
+        {
+          created_at: '2021-10-01T00:00:00.000000Z',
+          help_id: null,
+          id: 5,
+          price: 50,
+          update_at: '2021-10-01T00:00:00.000000Z',
+        },
+      ],
+    },
+  ],
+  error: null,
+};
+
+export const mockPricesListErrorRaw: PricesHelpsList = {
+  data: [],
+  error: {
+    message: 'error',
+    details: 'error',
+    hint: 'This is a hint',
+    code: '404',
+  },
+};
