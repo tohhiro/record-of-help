01ad4120b7070cc9a2567e0a2d83b1e3df250549
hiroto <tohhiro@gmail.com>
2024-09-07 14:06:42 +0900

    loginのバリデーションのテストを追加

---
+++ app/(feature)/login/helper/validationSchema/index.test.ts
@@ -0,0 +1,31 @@
+import { validationSchema } from '.';
+
+describe('validationSchema', () => {
+  test('有効なemailとpasswordの場合、バリデーションエラーが発生しない', () => {
+    const result = validationSchema.safeParse({ email: 'test@test.com', password: 'password' });
+    expect(result.success).toBeTruthy();
+  });
+  test('有効なemailでない場合、バリデーションエラーが発生する', () => {
+    const result = validationSchema.safeParse({ email: 'test', password: 'password' });
+    expect(result.success === false).toBeTruthy();
+    expect(result.success === false && result.error.issues).toContainEqual({
+      code: 'invalid_string',
+      message: 'Emailを入力してください。',
+      path: ['email'],
+      validation: 'email',
+    });
+  });
+  test('8文字未満のpasswordの場合、バリデーションが発生する', () => {
+    const result = validationSchema.safeParse({ email: 'test@test.com', password: '' });
+    expect(result.success === false).toBeTruthy();
+    expect(result.success === false && result.error.issues[0]).toStrictEqual({
+      code: 'too_small',
+      exact: false,
+      inclusive: true,
+      message: '8文字以上で入力してください。',
+      minimum: 8,
+      path: ['password'],
+      type: 'string',
+    });
+  });
+});
