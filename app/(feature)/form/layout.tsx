af56b5653db69348f689cd5494803b6a67817a49
hiroto <tohhiro@gmail.com>
2024-10-06 16:44:01 +0900

    pushされないため、layoutのtitleを修正してみて試す

--- app/(feature)/form/layout.tsx
+++ app/(feature)/form/layout.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 
 export const metadata = {
-  title: 'Record of Help | Record',
+  title: 'Record of Help | Form',
   description: 'This App is recording help by your children',
 };
 
