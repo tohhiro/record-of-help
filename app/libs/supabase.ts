c226e26a6d142903c822ee227e302ea555d68cea
hiroto <tohhiro@gmail.com>
2024-10-14 14:42:20 +0900

    フラグでsupabaseが起動するしないを切り替えるように試す

--- app/libs/supabase.ts
+++ app/libs/supabase.ts
@@ -1,6 +1,14 @@
+/* eslint-disable indent */
 import { createBrowserClient } from '@supabase/ssr';
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);
+export const supabase = !process.env.IS_CI
+  ? createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    )
+  : null;
+
+// export const supabase = createBrowserClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+// );
