b6dca8875ff92c395655ed1fbfe2e88913895c56
hiroto <tohhiro@gmail.com>
2024-10-14 16:27:41 +0900

    storybookのpreviewのmswの設定を削除

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,18 +4,6 @@ import { initialize, mswLoader, mswDecorator } from 'msw-storybook-addon';
 
 initialize();
 
-if (typeof window === 'undefined') {
-  // Node.js 環境ではサーバーモードを使う
-  const { setupServer } = require('msw/node');
-  const { handlers } = require('../src/mocks/handler/pricesList');
-
-  const server = setupServer(handlers);
-
-  beforeAll(() => server.listen());
-  afterEach(() => server.resetHandlers());
-  afterAll(() => server.close());
-}
-
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
