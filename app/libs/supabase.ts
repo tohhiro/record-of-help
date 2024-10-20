b4a6ca752550b0acc4405f1cb9dc6087f4d9a822
hiroto <tohhiro@gmail.com>
2024-10-20 17:52:51 +0900

    環境変数の箇所を修正

--- app/libs/supabase.ts
+++ app/libs/supabase.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr';
 
 export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 );
