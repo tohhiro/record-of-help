c3552084c79a94602ea6724d5c909c01f19f1bcd
hiroto <tohhiro@gmail.com>
2024-11-04 13:15:07 +0900

    ダッシュボードのカレンダー表示がズレていたのを修正

--- app/(feature)/dashboard/page.tsx
+++ app/(feature)/dashboard/page.tsx
@@ -45,8 +45,10 @@ const wageItem = ['dish', 'curtain', 'prepareEat', 'landry', 'special'];
 
 const getNowMonthFirstLast = () => {
   const nowDate = new Date();
-  const nowMonthFirst = new Date(nowDate.getFullYear(), nowDate.getMonth(), 1);
-  const nowMonthLast = new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, 0);
+  const year = nowDate.getFullYear();
+  const month = nowDate.getMonth();
+  const nowMonthFirst = new Date(year, month, 2);
+  const nowMonthLast = new Date(year, month + 1);
   return {
     startDate: nowMonthFirst.toISOString().split('T')[0],
     endDate: nowMonthLast.toISOString().split('T')[0],
