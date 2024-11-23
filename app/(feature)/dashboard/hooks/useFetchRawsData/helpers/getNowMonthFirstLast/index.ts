9aab892ee1259ed918e93c9768c076f2d1fd1524
hiroto <tohhiro@gmail.com>
2024-11-23 22:14:06 +0900

    CIでタイムゾーンが異なることが原因で落ちるので、TZを固定するように修正

--- app/(feature)/dashboard/hooks/useFetchRawsData/helpers/getNowMonthFirstLast/index.ts
+++ app/(feature)/dashboard/hooks/useFetchRawsData/helpers/getNowMonthFirstLast/index.ts
@@ -2,10 +2,25 @@ export const getNowMonthFirstLast = () => {
   const nowDate = new Date();
   const year = nowDate.getFullYear();
   const month = nowDate.getMonth();
-  const nowMonthFirst = new Date(year, month, 2);
-  const nowMonthLast = new Date(year, month + 1, 1);
+
+  const nowMonthFirst = new Date(year, month, 1);
+
+  const nextMonthFirst = new Date(year, month + 1, 1);
+  const nowMonthLast = new Date(nextMonthFirst.getTime() - 1); // 1ミリ秒前は当月末
+
+  // 日本時間のタイムゾーンを指定してフォーマット
+  const formatDateInJST = (date: Date) =>
+    new Intl.DateTimeFormat('ja-JP', {
+      timeZone: 'Asia/Tokyo',
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    })
+      .format(date)
+      .replaceAll('/', '-'); // YYYY-MM-DD形式に調整
+
   return {
-    startDate: nowMonthFirst.toISOString().split('T')[0],
-    endDate: nowMonthLast.toISOString().split('T')[0],
+    startDate: formatDateInJST(nowMonthFirst), // 当月1日
+    endDate: formatDateInJST(nowMonthLast), // 当月末日
   };
 };
