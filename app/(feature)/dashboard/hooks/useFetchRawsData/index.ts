ae31b8bef3fb4985d1b3419d9e963d05748b03e8
hiroto <tohhiro@gmail.com>
2024-11-30 20:54:05 +0900

    `useFetchRawsData`の`fecher`関数を`try/catch`でラップ

--- app/(feature)/dashboard/hooks/useFetchRawsData/index.ts
+++ app/(feature)/dashboard/hooks/useFetchRawsData/index.ts
@@ -1,7 +1,7 @@
 'use client';
 import { supabase } from '@/app/libs/supabase';
 import type { Database } from '@/supabase/schema';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import useSWR, { mutate } from 'swr';
 import { getNowMonthFirstLast } from './helpers';
 
@@ -14,25 +14,29 @@ export type ConditionsArgsType = {
 };
 
 const conditionsFetcher = async (args: ConditionsArgsType) => {
-  const commonSupabaseFetcher = supabase
-    .from('raws_data')
-    .select('*')
-    .order('created_at', { ascending: true });
+  try {
+    const commonSupabaseFetcher = supabase
+      .from('raws_data')
+      .select('*')
+      .order('created_at', { ascending: true });
 
-  const fetchPerson = () => {
-    if (!args.person) {
+    const fetchPerson = () => {
+      if (!args.person) {
+        return commonSupabaseFetcher
+          .gte('created_at', `${args.startDate} 00:00:00`)
+          .lte('created_at', `${args.endDate} 23:59:59`);
+      }
       return commonSupabaseFetcher
         .gte('created_at', `${args.startDate} 00:00:00`)
-        .lte('created_at', `${args.endDate} 23:59:59`);
-    }
-    return commonSupabaseFetcher
-      .gte('created_at', `${args.startDate} 00:00:00`)
-      .lte('created_at', `${args.endDate} 23:59:59`)
-      .eq('person', args.person);
-  };
+        .lte('created_at', `${args.endDate} 23:59:59`)
+        .eq('person', args.person);
+    };
 
-  const { data, error } = await fetchPerson();
-  return { data, error };
+    const { data, error } = await fetchPerson();
+    return { data, error };
+  } catch (error) {
+    throw new Error(String(error));
+  }
 };
 
 export const useFetchRawsData = () => {
@@ -45,16 +49,18 @@ export const useFetchRawsData = () => {
     endDate,
   };
 
-  const { data } = useSWR('raws_data', () => conditionsFetcher({ ...sendingData }));
+  const fetcher = () => conditionsFetcher({ ...sendingData });
+
+  const { data } = useSWR('raws_data', fetcher);
   useEffect(() => {
     setRawsData(data?.data || null);
   }, [data]);
 
-  const mutateFetch = async (args: ConditionsArgsType) => {
+  const mutateFetch = useCallback(async (args: ConditionsArgsType) => {
     const swrKey = `raws_data_conditions/${args.startDate}/${args.endDate}/${args.person}`;
     const result = await mutate(swrKey, conditionsFetcher({ ...args }));
     setRawsData(() => result?.data || null);
-  };
+  }, []);
 
   return {
     success: {
