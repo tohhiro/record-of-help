26dfba84314a3b3b93805f40af9d47fb6b73e663
hiroto <tohhiro@gmail.com>
2024-12-14 18:18:17 +0900

    ButtonコンポーネントをCVAからTailwind variantsに置き換え - cvaからtvに置き換え - 重複するプロパティを削除

--- app/components/Button/index.styles.ts
+++ app/components/Button/index.styles.ts
@@ -1,17 +1,16 @@
-import { cva, VariantProps } from 'class-variance-authority';
+import { tv } from 'tailwind-variants';
 
-export const buttonStyles = cva('font-bold py-2 px-4 rounded', {
+export const buttonStyles = tv({
+  base: 'font-bold py-2 px-4 rounded text-white bg-blue-500',
   variants: {
     intent: {
-      primary: 'bg-blue-500 hover:bg-blue-700 text-white ',
+      primary: 'hover:bg-blue-700  ',
       secondary:
         'bg-transparent hover:bg-blue-500 text-blue-700 border border-blue-500 hover:border-transparent',
-      disabled: 'bg-blue-500 text-white opacity-50 cursor-not-allowed text-white ',
+      disabled: 'opacity-50 cursor-not-allowed',
     },
   },
   defaultVariants: {
     intent: 'primary',
   },
 });
-
-export type ButtonVariantProps = VariantProps<typeof buttonStyles>;
