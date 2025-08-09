6fd5c3c51905e30474db59ee4b746ffe1d9ce8a1
hiroto <tohhiro@gmail.com>
2025-08-09 12:30:38 +0900

    コールバックをオプショナルに変更

--- app/(feature)/navHeader/hooks/useSingOut/index.ts
+++ app/(feature)/navHeader/hooks/useSingOut/index.ts
@@ -1,11 +1,11 @@
 import { supabase } from '@/app/libs/supabase';
 
 export const useSignOut = () => {
-  const signOut = async (cb: { onError: (_error: Error) => void }) => {
+  const signOut = async (cb?: { onError: (_error: Error) => void }) => {
     const { error } = await supabase.auth.signOut();
 
     if (error) {
-      cb.onError(error);
+      cb?.onError(error);
     }
   };
 
