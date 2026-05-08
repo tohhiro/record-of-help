c5e5e8624a38ec82b6a988c64afba4457b49a45f
tohhiro <tohhiro@gmail.com>
2026-05-08 18:40:38 +0900

    test: モック・Storybook の landry を laundry に修正
    - mocks のテストデータ
    - Table Storybook のデモデータ
    
    DB カラム名の変更に伴う確認用リソースの修正。

--- mocks/tableHeader.ts
+++ mocks/tableHeader.ts
@@ -2,7 +2,7 @@ export const mockThData = {
   person: '氏名',
   dish: '皿洗い',
   curtain: 'カーテン開閉',
-  landry: '洗濯物',
+  laundry: '洗濯物',
   prepareEat: '食事準備',
   special: 'スペシャル',
   comments: 'コメント',
