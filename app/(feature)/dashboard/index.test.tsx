0d3f60a57fc1e4cb0e81dc3cbef87beb7b2983e5
tohhiro <tohhiro@gmail.com>
2026-06-29 08:12:09 +0900

    dashboardのUTをsetup関数でrender、userEventを返すようにリファクタリング

--- app/(feature)/dashboard/index.test.tsx
+++ app/(feature)/dashboard/index.test.tsx
@@ -4,8 +4,14 @@ import { DashboardClient } from './DashboardClient';
 
 jest.mock('next/navigation', () => jest.requireActual('next-router-mock'));
 
+const setup = (jsx: JSX.Element) => {
+  return {
+    user: userEvent.setup(),
+    ...render(jsx),
+  };
+};
+
 describe('Dashboard', () => {
-  const user = userEvent.setup();
   const mockMemberOptions = [
     { value: 'all', label: 'All' },
     { value: 'eito', label: 'eito' },
@@ -19,13 +25,13 @@ describe('Dashboard', () => {
 
   describe('検索パネル', () => {
     test('SelectBoxが1つレンダリングされる', () => {
-      render(<DashboardClient memberOptions={mockMemberOptions} />);
+      setup(<DashboardClient memberOptions={mockMemberOptions} />);
       const select = screen.getAllByRole('combobox');
       expect(select).toHaveLength(1);
     });
 
     test('Inputがdate属性で2つレンダリングされる', () => {
-      render(<DashboardClient memberOptions={mockMemberOptions} />);
+      setup(<DashboardClient memberOptions={mockMemberOptions} />);
       const startInput = screen.getByLabelText('開始');
       const endInput = screen.getByLabelText('終了');
       [startInput, endInput].forEach((input) => {
@@ -35,7 +41,7 @@ describe('Dashboard', () => {
     });
 
     test('Buttonがsubmit属性、Enabledで1つレンダリングされる', () => {
-      render(<DashboardClient memberOptions={mockMemberOptions} />);
+      setup(<DashboardClient memberOptions={mockMemberOptions} />);
       const button = screen.getByRole('button', { name: '検索' });
       expect(button).toBeInTheDocument();
       expect(button).toHaveAttribute('type', 'submit');
@@ -43,7 +49,7 @@ describe('Dashboard', () => {
     });
 
     test('対象者を未選択、開始終了をクリアし検索ボタンを押すとvalidationエラーになる', async () => {
-      render(<DashboardClient memberOptions={mockMemberOptions} />);
+      const { user } = setup(<DashboardClient memberOptions={mockMemberOptions} />);
       const validationsText = ['対象を選択', '開始日を選択', '終了日を選択'];
       const button = screen.getByRole('button', { name: '検索' });
 
@@ -60,14 +66,14 @@ describe('Dashboard', () => {
 
     describe('非表示/表示ボタン', () => {
       test('非表示のボタンがレンダリングされる', () => {
-        render(<DashboardClient memberOptions={mockMemberOptions} />);
+        setup(<DashboardClient memberOptions={mockMemberOptions} />);
         const button = screen.getByRole('button', { name: '表示' });
         expect(button).toBeInTheDocument();
       });
 
       test('表示ボタンをクリックすると検索パネルが表示される', async () => {
         const labelNames = ['対象者を選択', '開始', '終了', '検索', '非表示'];
-        render(<DashboardClient memberOptions={mockMemberOptions} />);
+        const { user } = setup(<DashboardClient memberOptions={mockMemberOptions} />);
         const button = screen.getByRole('button', { name: '表示' });
 
         await user.click(button);
